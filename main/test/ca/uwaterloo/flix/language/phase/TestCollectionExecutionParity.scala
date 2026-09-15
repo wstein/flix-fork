fcaf03c4bd911b2ff800598c107f97d3f6e894b3
Werner Stein <werner.stein@gmail.com>
2026-09-15 23:11:56 +0200

    test: adapt sequential collection tests to compiler api

--- main/test/ca/uwaterloo/flix/language/phase/TestCollectionExecutionParity.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestCollectionExecutionParity.scala
@@ -19,6 +19,7 @@ package ca.uwaterloo.flix.language.phase
 import ca.uwaterloo.flix.TestUtils
 import ca.uwaterloo.flix.api.{CompilerConstants, Flix}
 import ca.uwaterloo.flix.language.ast.shared.SecurityContext
+import ca.uwaterloo.flix.runtime.JvmLoader
 import ca.uwaterloo.flix.util.{ExecutionMode, Options, Result}
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -36,9 +37,9 @@ class TestCollectionExecutionParity extends AnyFunSuite with TestUtils {
     val flix = new Flix().setOptions(options)
     implicit val sctx: SecurityContext = SecurityContext.Unrestricted
     flix.addVirtualPath(CompilerConstants.VirtualTestFile, src)
-    flix.compile().toResult match {
+    flix.compile() match {
       case Result.Ok(res) =>
-        val tests = res.getTests
+        val tests = JvmLoader.load(res).tests
         val (_, testFn) = tests.headOption.getOrElse(fail("No @Test found in compilation result"))
         testFn.run()
       case Result.Err(errors) =>
